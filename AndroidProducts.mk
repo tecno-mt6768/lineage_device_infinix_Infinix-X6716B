018506b101b3cc21a0c9298e5fde28a90bf061c1
isus203 <128573607+isus203@users.noreply.github.com>
2024-08-08 19:43:32 +0300

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_FULL-64.mk
+    $(LOCAL_DIR)/lineage_X6716B.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_FULL-64-user \
-    lineage_FULL-64-userdebug \
-    lineage_FULL-64-eng
+    lineage_X6716B-user \
+    lineage_X6716B-userdebug \
+    lineage_X6716B-eng
