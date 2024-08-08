6bb6f1181df3f01b739daff51161a0439c88209e
isus203 <128573607+isus203@users.noreply.github.com>
2024-08-08 19:36:33 +0300

    Update and rename lineage_FULL-64.mk to lineage_X6716B.mk

--- lineage_X6716B.mk
+++ lineage_X6716B.mk
@@ -11,11 +11,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-# Inherit from FULL-64 device
-$(call inherit-product, device/infinix/FULL-64/device.mk)
+# Inherit from X6716B device
+$(call inherit-product, device/infinix/X6716B/device.mk)
 
-PRODUCT_DEVICE := FULL-64
-PRODUCT_NAME := lineage_FULL-64
+PRODUCT_DEVICE := X6716B
+PRODUCT_NAME := lineage_X6716B
 PRODUCT_BRAND := Infinix
 PRODUCT_MANUFACTURER := infinix
 
@@ -24,4 +24,4 @@ PRODUCT_GMS_CLIENTID_BASE := android-infinix
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="sys_tssi_64_infinix-user 13 TP1A.220624.014 646430 release-keys"
 
-BUILD_FINGERPRINT := Infinix/TSSI/FULL-64:13/TP1A.220624.014/240626V893:user/release-keys
+BUILD_FINGERPRINT := Infinix/X6716B-GL/Infinix-X6716B:12/SP1A.210812.016/240626V897:user/release-keys
